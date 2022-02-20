4d1a0ef294dd365a3ef44938fa923f7c69dac9b1
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2022-02-20 14:12:06 +1100

    fixed breathing timing and cleanup

--- breath timer/breathing.js
+++ breath timer/breathing.js
@@ -1,13 +1,7 @@
-
-
 var breath_start = 0
-var breathing = 0
-
-
 
 function start_breathing() {
     breath_start = new Date();
-    breathing = true;
 
     breathing = window.setInterval(do_breathing, 30);
 }
@@ -18,24 +12,31 @@ function do_breathing() {
     var time_since_start = now - breath_start;
     var elapsed_seconds = time_since_start / 1000;
 
-    var r = (Math.sin(elapsed_seconds / 2.5) + 1) / 2 * 100;
-    console.log(r);
+    var cycles = elapsed_seconds * Math.PI * 2;
 
-    circle(r);
+    var r = (Math.cos(cycles / 10) + 1) / 2;
 
     if (elapsed_seconds >= 60) {
+        r = 1;
         clearInterval(breathing);
     }
+
+    circle(r * 250);
 }
 
 function circle(r) {
+    // canvas context
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
+
+    // white background
     ctx.beginPath();
     ctx.fillStyle = "#FFFFFF";
-    ctx.arc(250, 250, 100, 0, Math.PI * 2);
+    ctx.arc(250, 250, 250, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
+
+    // breath circle
     ctx.beginPath();
     ctx.fillStyle = "#8000FF";
     ctx.arc(250, 250, r, 0, 2 * Math.PI);
